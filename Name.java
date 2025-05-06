8309480ea7e5955e42da9987ce750cb2b0b7f18c
Karthik Subramanian <subramaniankarthik12@gmail.com>
2025-05-06 18:45:21 +0530

    added Name Program

---
+++ Name.java
@@ -0,0 +1,16 @@
+class Name
+{
+public static void main(String arg[])
+{
+    if((arg.length)>0)
+    {
+        String Name = arg[0];
+        
+        System.out.println("Welcome Home "+Name);
+    }
+    else{
+        System.out.println("No Arguments Passed");
+    }
+
+}
+}
